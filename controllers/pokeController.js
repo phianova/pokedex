3b5ba130f86ac0edb61045dac60e3990c1824e7f
phianova <98988580+phianova@users.noreply.github.com>
2024-01-22 16:26:01 +0000

    update functionality

--- controllers/pokeController.js
+++ controllers/pokeController.js
@@ -18,12 +18,40 @@ exports.deletePokemon = (req, res, next) => {
     //params can be dynamic value
     const pokemonId = Number(req.params.id)
     const pokemonIndex = pokemon.findIndex(pokemon => pokemon.id === pokemonId)
-    if (pokemonIndex === -1) return next(createError(404, "Todo Not Found"))
+    if (pokemonIndex === -1) return next(createError(404, "Pokemon Not Found"))
     pokemon.splice(pokemonIndex, 1)
     res.send(pokemon)
 }
 
+exports.updatePokemon = (req, res, next) => {
+    //get new pokemon information
+    if (!req.headers.secretstring || req.headers.secretstring !== 'password123') return next(createError(401, 'Unauthorized'))
+    const isMissingInformation = !req.body.name || !req.body.species || !req.body.type
+    if (isMissingInformation) return next(createError(400, 'Missing Pokemon Information')) 
+    req.body.id =  pokemon.length + 1 + Date.now();
+    console.log("new id",req.body.id);
+    //find old pokemon information
+    const oldPokemonId = Number(req.params.id)
+    console.log("old id",oldPokemonId);
+    const oldPokemonIndex = pokemon.findIndex(pokemon => pokemon.id === oldPokemonId)
+    console.log("old index",oldPokemonIndex);
+    if (oldPokemonIndex === -1) return next(createError(404, "Pokemon Not Found"))
+    //insert new info in place of old info
+    pokemon.splice(oldPokemonIndex, 1, req.body)
+    res.send(pokemon)
+}
 
+
+/*
+exports.evolvePokemon = (req, res, next) => {
+    const pokemonId = Number(req.params.id)
+    const pokemonIndex = pokemon.findIndex(pokemon => pokemon.id === pokemonId)
+    if (pokemonIndex === -1) return next(createError(404, "Pokemon Not Found"))
+    const currentSpecies = pokemon[pokemonIndex].species
+    //Codeium suggested: const evolveTo = pokemon.find(pokemon => pokemon.species === currentSpecies + 1)
+}
+
+*/
 
 // axios.post("http://localhost:3000/create", {
 //     name: "learn Node",
--- routes/pokeRoutes.js
+++ routes/pokeRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router();
-const { getAllPokemon, addPokemon, deletePokemon } = require("../controllers/pokeController")
+const { getAllPokemon, addPokemon, deletePokemon, updatePokemon } = require("../controllers/pokeController")
 
 router.get("/all", getAllPokemon) 
 router.post("/add", addPokemon)
 router.delete("/delete/:id", deletePokemon)
+router.put("/update/:id", updatePokemon)
 
 module.exports = router;
 
