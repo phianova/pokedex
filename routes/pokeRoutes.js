b0c0cb2475179cb5126558b3e52ab2759907effc
phianova <98988580+phianova@users.noreply.github.com>
2024-01-23 15:42:56 +0000

    evolve, rename, change, limit

--- routes/pokeRoutes.js
+++ routes/pokeRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router();
-const { getAllPokemon, addPokemon, deletePokemon, changePokemon, renamePokemon} = require("../controllers/pokeController")
+const { getAllPokemon, addPokemon, deletePokemon, changePokemon, renamePokemon, evolvePokemon} = require("../controllers/pokeController")
 
 router.get("/all", getAllPokemon) 
 router.post("/add/:species", addPokemon)
 router.delete("/delete/:id", deletePokemon)
-router.put("/change/:id", changePokemon)
+router.put("/change/:id/:species", changePokemon)
+router.put("/evolve/:id", evolvePokemon)
 
 // router.get("/pokemon/:species", getData)
   
