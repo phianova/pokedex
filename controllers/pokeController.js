151f655c505beefba0891cdc5b1860b6cbd14fc3
Dt8Profile <138679384+Dt8Profile@users.noreply.github.com>
2024-01-24 15:54:20 +0000

    connected database

--- controllers/pokeController.js
+++ controllers/pokeController.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const axios = require("axios");
 const Pokemon = require("../models/pokemon");
-// const pokemon = [];
 
 exports.getAllPokemon = async (req, res, next) => {
   try {
@@ -26,8 +25,7 @@ const getData = async (req, res, next) => {
   const evolutionResponse = await evolutionData(req, res, next);
   const apiData = { mainData, evolutionResponse };
   if (!apiData) {
-    //throw new Error("Pokemon Not Found in pokeAPI");
-    return;
+    throw new Error("Pokemon Not Found in pokeAPI");
   } else {
     return apiData;
   }
@@ -50,17 +48,17 @@ const evolutionData = async (req, res, next) => {
 
 exports.addPokemon = async (req, res, next) => {
   try {
-    const numberOfPokemon = await Pokemon.find().length;
+    const numberOfPokemon = await Pokemon.countDocuments();
     if (numberOfPokemon >= 6)
       return next(
         createError(400, "Pokemon Limit Reached - please swap or delete")
       );
-    if (!req.headers.secretstring || req.headers.secretstring !== "password123")
-      return next(createError(401, "Unauthorized"));
+    // if (!req.headers.secretstring || req.headers.secretstring !== "password123")
+    //   return next(createError(401, "Unauthorized"));
     const isMissingInformation = !req.body.nickname || !req.params.species;
     if (isMissingInformation)
       return next(createError(400, "Missing Pokemon Information"));
-    req.body.id = pokemon.length + 1 + Date.now();
+
     const allData = await getData(req, res, next);
     const pokemonData = allData.mainData;
     const pokemonTypes = () => {
@@ -70,52 +68,22 @@ exports.addPokemon = async (req, res, next) => {
       }
       return types;
     };
+
     req.body.type = pokemonTypes();
     req.body.height = pokemonData.height;
     req.body.weight = pokemonData.weight;
     req.body.image = pokemonData.sprites.front_default;
     req.body.pokeId = pokemonData.id;
     req.body.species = pokemonData.species.name;
-    let currentSpecies = req.body.species;
-    let nextSpecies = currentSpecies;
 
-    if (allData.evolutionResponse.chain.evolves_to.length > 0) {
-      if (currentSpecies === allData.evolutionResponse.chain.species.name) {
-        nextSpecies =
-          allData.evolutionResponse.chain.evolves_to[0].species.name;
-      } else {
-        if (
-          allData.evolutionResponse.chain.evolves_to[0].evolves_to.length > 0
-        ) {
-          if (
-            currentSpecies ===
-            allData.evolutionResponse.chain.evolves_to[0].species.name
-          ) {
-            nextSpecies =
-              allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                .species.name;
-          } else {
-            if (
-              allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                .evolves_to.length > 0
-            ) {
-              if (
-                currentSpecies ===
-                allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                  .species.name
-              ) {
-                nextSpecies =
-                  allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                    .evolves_to[0].species.name;
-              }
-            }
-          }
-        }
-      }
-    }
+    const nextSpecies = determineNextSpecies(
+      allData.evolutionResponse.chain,
+      req.body.species
+    );
+
     req.body.evolvesTo = nextSpecies;
-    console.log(req.body);
-    const pokemon = new Pokemon({
+
+    const newPokemon = new Pokemon({
       type: req.body.type,
       height: req.body.height,
       weight: req.body.weight,
@@ -125,172 +93,158 @@ exports.addPokemon = async (req, res, next) => {
       species: req.body.species,
       evolvesTo: req.body.evolvesTo,
     });
-    await pokemon.save();
-    res.send(pokemon);
+
+    await newPokemon.save();
+    res.send(newPokemon);
   } catch (err) {
     return next(createError(500, err.message));
   }
-  // pokemon.push(req.body)
-  // res.send(pokemon)
 };
 
-exports.deletePokemon = (req, res, next) => {
-  //params can be dynamic value
-  const pokemonId = Number(req.params.id);
-  const pokemonIndex = pokemon.findIndex((pokemon) => pokemon.id === pokemonId);
-  if (pokemonIndex === -1) return next(createError(404, "Pokemon Not Found"));
-  pokemon.splice(pokemonIndex, 1);
-  res.send(pokemon);
-};
-
-exports.changePokemon = async (req, res, next) => {
-  //get new pokemon information
-  if (!req.headers.secretstring || req.headers.secretstring !== "password123")
-    return next(createError(401, "Unauthorized"));
-  const isMissingInformation = !req.body.name || !req.params.species;
-  if (isMissingInformation)
-    return next(createError(400, "Missing Pokemon Information"));
-  req.body.id = pokemon.length + 1 + Date.now();
-  //find old pokemon information
-  const oldPokemonId = Number(req.params.id);
-  const oldPokemonIndex = pokemon.findIndex(
-    (pokemon) => pokemon.id === oldPokemonId
-  );
-  if (oldPokemonIndex === -1)
-    return next(createError(404, "Pokemon Not Found"));
-  //insert new info in place of old info
-  const allData = await getData(req, res, next);
-  const pokemonData = allData.mainData;
-  const pokemonTypes = () => {
-    let types = [];
-    for (let i = 0; i < pokemonData.types.length; i++) {
-      types.push(pokemonData.types[i].type.name);
+function determineNextSpecies(evolutionChain, currentSpecies) {
+  if (evolutionChain.species.name === currentSpecies) {
+    if (evolutionChain.evolves_to.length > 0) {
+      return evolutionChain.evolves_to[0].species.name;
     }
-    return types;
-  };
-  pokemon[oldPokemonIndex].type = pokemonTypes();
-  pokemon[oldPokemonIndex].height = pokemonData.height;
-  pokemon[oldPokemonIndex].weight = pokemonData.weight;
-  pokemon[oldPokemonIndex].image = pokemonData.sprites.front_default;
-  pokemon[oldPokemonIndex].pokeId = pokemonData.id;
-  pokemon[oldPokemonIndex].species = req.params.species;
-  let currentSpecies = pokemon[oldPokemonIndex].species;
-  let nextSpecies = currentSpecies;
-
-  if (allData.evolutionResponse.chain.evolves_to.length > 0) {
-    if (currentSpecies === allData.evolutionResponse.chain.species.name) {
-      nextSpecies = allData.evolutionResponse.chain.evolves_to[0].species.name;
-    } else {
-      if (allData.evolutionResponse.chain.evolves_to[0].evolves_to.length > 0) {
+  } else {
+    // for (const evolution of evolutionChain.evolves_to) {
+    //   const nextSpecies = determineNextSpecies(evolution, currentSpecies);
+    //   if (nextSpecies) {
+    //     return nextSpecies;
+    //   }
+    // }
+    if (currentSpecies === evolutionChain.evolves_to[0].species.name) {
+      if (evolutionChain.evolves_to[0].evolves_to.length > 0) {
+        return evolutionChain.evolves_to[0].evolves_to[0].species.name;
+      } else {
         if (
           currentSpecies ===
-          allData.evolutionResponse.chain.evolves_to[0].species.name
+          evolutionChain.evolves_to[0].evolves_to[0].species.name
         ) {
-          nextSpecies =
-            allData.evolutionResponse.chain.evolves_to[0].evolves_to[0].species
-              .name;
-        } else {
           if (
-            allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-              .evolves_to.length > 0
+            evolutionChain.evolves_to[0].evolves_to[0].evolves_to.length > 0
           ) {
-            if (
-              currentSpecies ===
-              allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                .species.name
-            ) {
-              nextSpecies =
-                allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                  .evolves_to[0].species.name;
-            }
+            return evolutionChain.evolves_to[0].evolves_to[0].evolves_to[0]
+              .species.name;
           }
         }
       }
     }
   }
-  pokemon[oldPokemonIndex].evolvesTo = nextSpecies;
-  res.send(pokemon);
+  return currentSpecies;
+}
+
+exports.deletePokemon = async (req, res, next) => {
+  try {
+    const pokemonToDelete = await Pokemon.findByIdAndDelete(req.params.id);
+    if (!pokemonToDelete) {
+      return next(createError(404, "no pokemon with that id"));
+    }
+    res.send({ result: true });
+  } catch (err) {
+    return next(createError(500, err.message));
+  }
 };
 
-exports.renamePokemon = (req, res, next) => {
-  const pokemonId = Number(req.params.id);
-  const pokemonIndex = pokemon.findIndex((pokemon) => pokemon.id === pokemonId);
-  if (pokemonIndex === -1) return next(createError(404, "Pokemon Not Found"));
-  pokemon[pokemonIndex].nickname = req.body.nickname;
-  res.send(pokemon);
+// exports.changePokemon = async (req, res, next) => {
+//   //get new pokemon information
+//   //   if (!req.headers.secretstring || req.headers.secretstring !== "password123")
+//   //     return next(createError(401, "Unauthorized"));
+//   const isMissingInformation = !req.body.name || !req.params.species;
+//   if (isMissingInformation)
+//     return next(createError(400, "Missing Pokemon Information"));
+//   //find old pokemon information
+//   const oldPokemonId = await Pokemon.findById(req.params.id);
+//   const oldPokemonIndex = pokemon.findIndex(
+//     (pokemon) => pokemon.id === oldPokemonId
+//   );
+//   if (oldPokemonIndex === -1)
+//     return next(createError(404, "Pokemon Not Found"));
+//   //insert new info in place of old info
+//   const allData = await getData(req, res, next);
+//   const pokemonData = allData.mainData;
+//   const pokemonTypes = () => {
+//     let types = [];
+//     for (let i = 0; i < pokemonData.types.length; i++) {
+//       types.push(pokemonData.types[i].type.name);
+//     }
+//     return types;
+//   };
+//   pokemon[oldPokemonIndex].type = pokemonTypes();
+//   pokemon[oldPokemonIndex].height = pokemonData.height;
+//   pokemon[oldPokemonIndex].weight = pokemonData.weight;
+//   pokemon[oldPokemonIndex].image = pokemonData.sprites.front_default;
+//   pokemon[oldPokemonIndex].pokeId = pokemonData.id;
+//   pokemon[oldPokemonIndex].species = req.params.species;
+//   let currentSpecies = pokemon[oldPokemonIndex].species;
+//   const nextSpecies = determineNextSpecies(
+//     allData.evolutionResponse.chain,
+//     req.body.species
+//   );
+
+//   pokemon[oldPokemonIndex].evolvesTo = nextSpecies;
+//   res.send(pokemon);
+// };
+
+exports.renamePokemon = async (req, res, next) => {
+  try {
+    const updatedName = { nickname: req.body.nickname };
+    const pokemonToRename = await Pokemon.findByIdAndUpdate(
+      req.params.id,
+      updatedName
+    );
+    if (!pokemonToRename) {
+      return next(createError(404, "no pokemon with that id"));
+    }
+    res.send(pokemonToRename);
+  } catch (err) {
+    return next(createError(404, "Could not rename"));
+  }
 };
 
 exports.evolvePokemon = async (req, res, next) => {
-  //get unique id from req.params, fetch our req.body data
-  const pokemonId = Number(req.params.id);
-  const pokemonIndex = pokemon.findIndex((pokemon) => pokemon.id === pokemonId);
-  if (pokemonIndex === -1) return next(createError(404, "Pokemon Not Found"));
-  //get new data for evolved species
-  const evolvedSpecies = pokemon[pokemonIndex].evolvesTo;
-  req.params.species = evolvedSpecies;
-  const allData = await getData(req, res, next);
-  const pokemonData = allData.mainData;
-  //change req.body data based on new data
-  const pokemonTypes = () => {
-    let types = [];
-    for (let i = 0; i < pokemonData.types.length; i++) {
-      types.push(pokemonData.types[i].type.name);
-    }
-    return types;
-  };
-  pokemon[pokemonIndex].type = pokemonTypes();
-  pokemon[pokemonIndex].height = pokemonData.height;
-  pokemon[pokemonIndex].weight = pokemonData.weight;
-  pokemon[pokemonIndex].image = pokemonData.sprites.front_default;
-  pokemon[pokemonIndex].pokeId = pokemonData.id;
-  pokemon[pokemonIndex].species = req.params.species;
-  let currentSpecies = pokemon[pokemonIndex].species;
-  let nextSpecies = currentSpecies;
-
-  if (allData.evolutionResponse.chain.evolves_to.length > 0) {
-    if (currentSpecies === allData.evolutionResponse.chain.species.name) {
-      nextSpecies = allData.evolutionResponse.chain.evolves_to[0].species.name;
-    } else {
-      if (allData.evolutionResponse.chain.evolves_to[0].evolves_to.length > 0) {
-        if (
-          currentSpecies ===
-          allData.evolutionResponse.chain.evolves_to[0].species.name
-        ) {
-          nextSpecies =
-            allData.evolutionResponse.chain.evolves_to[0].evolves_to[0].species
-              .name;
-        } else {
-          if (
-            allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-              .evolves_to.length > 0
-          ) {
-            if (
-              currentSpecies ===
-              allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                .species.name
-            ) {
-              nextSpecies =
-                allData.evolutionResponse.chain.evolves_to[0].evolves_to[0]
-                  .evolves_to[0].species.name;
-            }
-          }
-        }
+  try {
+    const pokemonToEvolve = await Pokemon.findById(req.params.id);
+    const evolvedSpecies = pokemonToEvolve.evolvesTo;
+    req.params.species = evolvedSpecies;
+    const allData = await getData(req, res, next);
+    const pokemonData = allData.mainData;
+    const pokemonTypes = () => {
+      let types = [];
+      for (let i = 0; i < pokemonData.types.length; i++) {
+        types.push(pokemonData.types[i].type.name);
       }
+      return types;
+    };
+    pokemonToEvolve.type = pokemonTypes();
+    pokemonToEvolve.height = pokemonData.height;
+    pokemonToEvolve.weight = pokemonData.weight;
+    pokemonToEvolve.image = pokemonData.sprites.front_default;
+    pokemonToEvolve.pokeId = pokemonData.id;
+    pokemonToEvolve.species = req.params.species;
+    let currentSpecies = pokemonToEvolve.species;
+    const nextSpecies = determineNextSpecies(
+      allData.evolutionResponse.chain,
+      pokemonToEvolve.species
+    );
+
+    pokemonToEvolve.evolvesTo = nextSpecies;
+    if (currentSpecies === nextSpecies) {
+      console.log("No evolution");
     }
-  }
-  pokemon[pokemonIndex].evolvesTo = nextSpecies;
-  if (currentSpecies === nextSpecies) {
-    console.log("No evolution");
-  }
 
-  res.send(pokemon);
+    const evolvedPokemon = await Pokemon.findByIdAndUpdate(req.params.id, {
+      type: pokemonToEvolve.type,
+      height: pokemonToEvolve.height,
+      weight: pokemonToEvolve.weight,
+      image: pokemonToEvolve.image,
+      pokeId: pokemonToEvolve.pokeId,
+      species: pokemonToEvolve.species,
+      evolvesTo: pokemonToEvolve.evolvesTo,
+    });
+
+    res.send(evolvedPokemon);
+  } catch (err) {
+    return next(createError(404, "Could not evolve"));
+  }
 };
-
-// axios.post("http://localhost:3000/create", {
-//     name: "learn Node",
-//     expiry: "2021-12-31",
-//     difficultyLevel: 1
-// })
-// module.exports = {
-
-// }
--- routes/pokeRoutes.js
+++ routes/pokeRoutes.js
@@ -1,13 +1,18 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const { getAllPokemon, addPokemon, deletePokemon, changePokemon, renamePokemon, evolvePokemon} = require("../controllers/pokeController")
+const {
+  getAllPokemon,
+  addPokemon,
+  deletePokemon,
+  renamePokemon,
+  evolvePokemon,
+} = require("../controllers/pokeController");
 
-router.get("/all", getAllPokemon) 
-router.post("/add/:species", addPokemon)
-router.delete("/delete/:id", deletePokemon)
-router.put("/change/:id/:species", changePokemon)
-router.put("/evolve/:id", evolvePokemon)  
-router.put("/rename/:id", renamePokemon)
+router.get("/all", getAllPokemon);
+router.post("/add/:species", addPokemon);
+router.delete("/delete/:id", deletePokemon);
+//router.put("/change/:id/:species", changePokemon)
+router.put("/evolve/:id", evolvePokemon);
+router.put("/rename/:id", renamePokemon);
 
 module.exports = router;
-
