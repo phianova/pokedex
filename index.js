3eaf159635839fa6faee6843dd6c586654c2f546
phianova <98988580+phianova@users.noreply.github.com>
2024-01-26 10:36:11 +0000

    port listening

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require("./routes/pokeRoutes");
 const mongoose = require("mongoose");
 const cors = require("cors");
